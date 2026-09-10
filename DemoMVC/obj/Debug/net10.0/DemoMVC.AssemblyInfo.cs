cb7ed4ed1cdfe812bbf03cc7930f384549712497
NgTus <ngoctu08012004@gmail.com>
2026-09-10 15:15:09 +0700

    CSS

--- DemoMVC/obj/Debug/net10.0/DemoMVC.AssemblyInfo.cs
+++ DemoMVC/obj/Debug/net10.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+402b5f37c60e440ad117d954cc13e99b1f443c0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6329611711aba627d7fd748ac0fcb003bdf1b12")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
